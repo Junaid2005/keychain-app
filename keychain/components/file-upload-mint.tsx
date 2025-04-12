95b7a3805cc02f8c5dd82cb97b9dd979a9ceef84
Junaid <jm355010@gmail.com>
2025-04-12 23:28:40 +0100

    fixed build

--- keychain/components/file-upload-mint.tsx
+++ keychain/components/file-upload-mint.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 interface FileUploadType {
-  json?: any;
+  json: Record<string, unknown>;
 }
 
 export function FileUpload({ json }: FileUploadType) {
